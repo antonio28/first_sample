3668aaa9c6a428f6c6cf1c824a3870939063de56
Antonio <antonio@kindria.com>
2014-07-25 14:35:50 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,15 +10,20 @@ class User < ActiveRecord::Base
 has_secure_password
 validates :password, length: { minimum: 6 }
 
-def User.new_remember_token
-    SecureRandom.urlsafe_base64
-  end
+    def feed
+      # This is preliminary. See "Following users" for the full implementation.
+      Micropost.where("user_id = ?", id)
+    end
 
-  def User.digest(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
+    def User.new_remember_token
+      SecureRandom.urlsafe_base64
+    end
 
-  private
+    def User.digest(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
 
     def create_remember_token
       self.remember_token = User.digest(User.new_remember_token)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,12 +18,13 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
-
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
 
-describe "with admin attribute set to 'true'" do
+  describe "with admin attribute set to 'true'" do
     before do
       @user.save!
       @user.toggle!(:admin)
@@ -61,10 +62,10 @@ describe "with admin attribute set to 'true'" do
       addresses.each do |valid_address|
         @user.email = valid_address
         expect(@user).to be_valid
-end
-end
-end
-describe "when email address is already taken" do
+      end
+    end
+  end
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -72,11 +73,11 @@ describe "when email address is already taken" do
     end
 
     it { should_not be_valid }
-end
+  end
 
-it { should respond_to(:authenticate) }
+  it { should respond_to(:authenticate) }
 
-describe "when password is not present" do
+  describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
                        password: " ", password_confirmation: " ")
@@ -89,7 +90,7 @@ describe "when password is not present" do
     it { should_not be_valid }
   end
 
-describe "with a password that's too short" do
+  describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
@@ -112,5 +113,39 @@ describe "with a password that's too short" do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
   end
 end
